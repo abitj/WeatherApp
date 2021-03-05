8d6e6d79b22f03aab5918d45bc7617bfd6c0be1c
abitj <https://github.com/abitj>
2021-03-05 01:12:46 -0600

    built weather app

---
+++ WeatherApp/Program.cs
@@ -0,0 +1,23 @@
+﻿using Newtonsoft.Json.Linq;
+using System;
+using System.IO;
+
+namespace WeatherApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string key = File.ReadAllText("appsettings.json");
+            string APIkey = JObject.Parse(key).GetValue("DefaultKey").ToString();
+
+            Console.WriteLine("Please enter your zipcode here:");
+            string zipCode = Console.ReadLine();
+
+            string apiCall = $"https://api.openweathermap.org/data/2.5/weather?zip={zipCode}&units=imperial&appid={APIkey}";
+            Console.WriteLine();
+            Console.WriteLine($"It is currently {WeatherMap.GetTemp(apiCall)} °F in your area.");
+
+        }
+    }
+}
